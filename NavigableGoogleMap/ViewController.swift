f8b6adc04dfadc19dc6ba3cb380a0531b8c946bb
Avihu Turzion <avihu.turzion@gmail.com>
2018-03-10 23:42:56 +0200

    Created a search field

--- NavigableGoogleMap/ViewController.swift
+++ NavigableGoogleMap/ViewController.swift
@@ -9,9 +9,13 @@
 import GoogleMaps
 import UIKit
 
+private let defaultZoomLevel: Float = 18
+
 class ViewController: UIViewController {
     
     @IBOutlet private weak var mapView: GMSMapView!
+    @IBOutlet private weak var searchFieldContainer: UIView!
+    @IBOutlet private weak var locationSearchField: UITextField!
     
     private let locationManager = CLLocationManager()
     
@@ -20,11 +24,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMapView()
+        setupSearchFieldContainer()
         setupLocationServices()
     }
 }
 
+// MARK: - Internal
+
 private extension ViewController {
+    private func setupSearchFieldContainer() {
+        searchFieldContainer.layer.shadowColor = UIColor.black.cgColor
+        searchFieldContainer.layer.shadowOpacity = 0.2
+        searchFieldContainer.layer.shadowRadius = 5.0
+        searchFieldContainer.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
     private func setupLocationServices() {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
@@ -43,18 +57,32 @@ private extension ViewController {
     }
 }
 
+// MARK: - UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+}
+
+// MARK: - GMSMapViewDelegate
+
 extension ViewController: GMSMapViewDelegate {
     func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
         guard let coordinate = locationManager.location?.coordinate else { return false }
-        mapView.animate(to: GMSCameraPosition.camera(withTarget: coordinate, zoom: 18.0))
+        mapView.animate(to: GMSCameraPosition.camera(withTarget: coordinate, zoom: defaultZoomLevel))
         return true
     }
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        guard locationSearchField.isFirstResponder else { return }
+        locationSearchField.resignFirstResponder()
+    }
 }
+
+// MARK: - CLLocationManagerDelegate
 
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let coordinate = locations.first?.coordinate else { return }
-        mapView.camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 18.0)
+        mapView.camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: defaultZoomLevel)
         manager.stopUpdatingLocation()
     }
 }
